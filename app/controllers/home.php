5292ab3371f99051a4f8849960f5956c13373287
Rafał Szczerski <rafal.szczerski@gmail.com>
2015-01-17 02:16:23 +0100

    url parsing

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -1,6 +1,6 @@
 <?php
 class Home extends Controller {
     public function index() {
-        echo 'home/huj';
+        echo 'home/index';
     }
 }
--- app/core/App.php
+++ app/core/App.php
@@ -1,21 +1,38 @@
 <?php
-class App {
-    
-    protected $controller = 'home';
-    
-    protected $method = 'index';
-    
+
+class App {    
+    protected $controller = 'home';    
+    protected $method = 'index';    
     protected $params = array();
             
     function __construct() {
-        $this->parseURL();
+        $url = $this->parseUrl();
+        
+        print_r($url);
+        
+        if(file_exists('../app/controllers/' . $url[0] . '.php')) {
+            $this->controller = $url[0];
+            unset($url[0]);            
+        }
+            require_once '../app/controllers/' . $this->controller . '.php';            
+            $this->controller = new $this->controller;
+            
+            if(isset($url[1])) {
+                if(method_exists($this->controller, $url[1])) {
+                    $this->method = $url[1];
+                    unser($url[1]);
+                }
+            }
+            
+            $this->params = $url ? array_values($url) : array();
+            
+            call_user_func_array([$this->controller, $this->method], $this->params);
     }
 
-    public function parseURL() {        
+    public function parseUrl() {        
         if(isset($_GET['url'])) {
-            var_dump($_GET);
-        } else {
-            echo "chuj!";
+            $url = filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL);
+            return $url = explode('/', $url);
         }
     }
 }
