6d0c0be933588c4735405eecd8c005f81961275c
Rafał Szczerski <rafal.szczerski@gmail.com>
2015-01-17 12:51:07 +0100

    fix

---
+++ app/core/Auth.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Authorisation
+ */
+
+class Auth {
+    public static function handleLogged() {
+        session_start();
+        $checkLog = $_SESSION['loggedIn'];
+        if($checkLog == false) {
+            session_destroy();
+            header("location: ..login");
+            exit;
+        }
+    }
+}
